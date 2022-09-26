8eae05813b99f6e9d1b24ae2c70750142f0284aa
VikaGre <vikagredychian@gmail.com>
2022-09-26 15:40:45 +0300

    Initial commit

---
+++ Lesson1/HW1.py
@@ -0,0 +1,19 @@
+# task 2
+# time_input = int(input("Input the time in seconds: "))
+# hours = time_input // 3600
+# minutes = (time_input - hours * 3600) // 60
+# seconds = (time_input - hours * 3600 - minutes * 60)
+# print(f"The time is {hours}:{minutes}:{seconds}")
+
+# task 3
+# input_number_n = int(input("Input the number n: "))
+# decimal_n = input_number_n * 10 + input_number_n
+# hundredth_n = decimal_n * 10 + input_number_n
+# print("The result of n + nn + nnn is: ", input_number_n + decimal_n + hundredth_n)
+
+# task 4
+input_number = int(input("Input the number: "))
+
+
+
+
---
+++ Lesson1/lesson1.py
@@ -0,0 +1,16 @@
+age = "12"
+new_age = int(age)
+
+name = input("Input your name: ")  # input the string
+print(name)
+
+some_age = 5
+if some_age > 18:
+    print("You are more than 18")
+else:
+    print("You are 18 or less")
+
+n = 1
+while n <= 10:
+    print("jjj")
+    n += 1
